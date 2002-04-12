3b8ffaba01fb1ba422612b5a1c962a35d00af965
Cosimo Streppone <cosimo@cpan.org>
2002-04-12 05:13:53 +0000

    provide a decent default filename also for windoze
    git-svn-id: svn://localhost:81/cpan/perl5-device-modem/trunk@36 4c4f4632-356f-5f4b-b69a-e41fa93810c7

--- lib/Device/Modem/Log/File.pm
+++ lib/Device/Modem/Log/File.pm
@@ -1,13 +1,15 @@
-# $Id: File.pm,v 1.4 2002-04-09 22:18:17 cosimo Exp $
+# $Id: File.pm,v 1.5 2002-04-12 05:13:53 cosimo Exp $
 package Device::Modem::Log::File;
-$VERSION = substr q$Revision: 1.4 $, 10;
+$VERSION = substr q$Revision: 1.5 $, 10;
 
 use strict;
 use warnings;
 
 sub new {
 	my( $class, $package, $filename ) = @_;
-	$filename ||= '/var/log/modem.log';
+
+	# Get a decent default if no file available
+	$filename ||= default_filename();
 
 	my %obj = (
 		file => $filename,
@@ -15,6 +17,24 @@ sub new {
 	);
 
 	bless \%obj, 'Device::Modem::Log::File';
+}
+
+# provide a suitable filename default
+sub default_filename() {
+	my $cDir = '/var/log';
+
+	# If this is windoze (XXX 2000/XP? WINBOOTDIR?)
+	if( $^O =~ /Win/i ) {
+		$cDir = $ENV{'WINBOOTDIR'} || '/windows';
+		$cDir .= '/temp';
+	}
+
+	return $cDir.'/modem.log';
+}
+
+sub filename {
+	my $self = shift();
+	$self->{'file'};
 }
 
 sub write($$) {
@@ -53,7 +73,8 @@ This is meant for an example log class to be hooked to Device::Modem
 to provide one's favourite logging mechanism.
 You just have to implement your own C<new()>, C<write()> and C<close()> methods.
 
-Default text file is C</var/log/modem.log>.
+Default text file is C</var/log/modem.log>. On Windows platforms, this
+goes into C<[WINDOWS]/temp/modem.log>.
 
 Loaded automatically by B<Device::Modem> class when an object
 is instantiated, and it is the B<default> logging mechanism for
