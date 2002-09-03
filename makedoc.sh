8e58e7042996bf87f4efe843598c5a378ffd7593
Cosimo Streppone <cosimo@cpan.org>
2002-09-03 20:00:32 +0000

    added translation of email address
    git-svn-id: svn://localhost:81/cpan/perl5-device-modem/trunk@64 4c4f4632-356f-5f4b-b69a-e41fa93810c7

--- makedoc.sh
+++ makedoc.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+# $Id: makedoc.sh,v 1.2 2002-09-03 20:00:32 cosimo Exp $
+#
 
 mv README README.old
 pod2text Modem.pm >README
@@ -6,3 +9,4 @@ pod2text Modem.pm >README
 mv Modem.html Modem.html.old
 pod2html Modem.pm >Modem.html
 
+perl -i.bak -pe 's/dev\.xproject\.org/cpan.org/g' Modem.html
